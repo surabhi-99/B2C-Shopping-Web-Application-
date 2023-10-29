2dc38ec00aac788f3fa86c139a27febb411d2a34
Surabhi Thakur <61082171+surabhi-99@users.noreply.github.com>
2023-10-29 22:55:59 +0530

    changes

--- Ecommerce Web App/frontend/src/component/layout/Contact/Contact.js
+++ Ecommerce Web App/frontend/src/component/layout/Contact/Contact.js
@@ -5,8 +5,8 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: mymailforabhi@gmail.com</Button>
+      <a className="mailBtn" href="mailto:surabhithakur.60@gmail.com">
+        <Button>Contact: surabhithakur.60@gmail.com</Button>
       </a>
     </div>
   );
